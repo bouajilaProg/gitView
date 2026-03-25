2f79573c04e2a9b81acff53e9ce4fb471e703b99
bouajilaprog <bouajilamedyessine@gmail.com>
2026-03-25 11:46:30 +0100

    refactor: put tailwind in it own file

---
+++ src/core/viewer/tailwind.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        theme: {
+          bg: 'var(--bg)',
+          panel: 'var(--panel)',
+          border: 'var(--border)',
+          text: 'var(--text)',
+          muted: 'var(--muted)',
+          dim: 'var(--dim)',
+          blue: 'var(--blue)',
+          green: 'var(--green)',
+          yellow: 'var(--yellow)',
+          red: 'var(--red)',
+          magenta: 'var(--magenta)',
+          cyan: 'var(--cyan)',
+          orange: 'var(--orange)'
+        }
+      }
+    }
+  }
+}
+
